0934a5e97912c27d31aef4f4b2b8b12f61d9f797
blanga021 <blanga021@student.wethinkcode.co.za>
2022-08-06 20:57:12 +0200

    commit

--- sign_up.py
+++ sign_up.py
@@ -1,19 +1,42 @@
 import streamlit as stream
+import sqlite3 as sql
 
 
 def load_page():
 
-   username = stream.text_input(
-         "Username: ", "Username", 50,
-         "user-username", "default", "e.g username123")
+   name = stream.text_input(
+         "Name: ", "Your name here", 20,
+         "user-name", "default",)
+
+   surname = stream.text_input(
+         "Surname: ", "Your surname here", 20,
+         "user-surname", "default",)
+
+   age = stream.slider("Age: ", 0, 40, 18,)
+
+   email = stream.text_input(
+         "Email: ", "Your email here", 100,
+         "user-email", "default",)
 
    password = stream.text_input("password: ", "Password", 50,
             "user-password", "password")
 
-   infor = username, password
+   preferred_location = stream.radio("Preferred Location", ["Remote", "Physical"])
 
-   stream.button("Sign Up", "SignUpButtton", "Save Details", save_applicant_details,infor)
+   employment_type = stream.radio("Employment type", ["Permanent", "Internship", "Learnership", "Temporal"])
 
+   citizenship_status = stream.radio("Citizenship Status", ["South African", "Other"])
+
+   if citizenship_status == "Other":
+      citizenship_status = stream.text_input(
+         "Citizenship: ", "Your citizenship here", 20,
+         "user-citizenship", "default",)
+
+   infor = name, surname, age, email, password, preferred_location, employment_type, citizenship_status
+
+   save_applicant_details(infor)
 
 def save_applicant_details(applicant_info:list):
-   pass
+   if stream.button("Save"):
+      with sql.connect("file:database.db?mode=rwc", uri=True) as connection:
+         connection.execute("INSERT INTO")
