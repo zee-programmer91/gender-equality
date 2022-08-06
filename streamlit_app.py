dd54322e270185a240380bd7f1561439339eee35
blanga021 <blanga021@student.wethinkcode.co.za>
2022-08-06 16:06:24 +0200

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,16 @@ import streamlit as stream
 from sign_in import authenticate
 
 
-def sign_in():
-   authenticate()
-   pass
+def run_sign_in():
+   stream.title("Equality Is The Goal Now")
+
+   stream.write(
+   """This fight is an ongoing thing that we as an organization would like you to be part of as we fight together
+   to get closer to the goal that we as women have been striving for - EQUALITY!
+   """)
+
+   stream.button("Sign In", "SignIn", "Click here to sign in", authenticate)
 
 
-stream.title("Equality Is The Goal Now")
-
-stream.write(
-"""This fight is an ongoing thing that we as an organization would like you to be part of as we fight together
-to get closer to the goal that we as women have been striving for - EQUALITY!
-""")
-
-stream.button("Sign In", "SignIn", "Click here to sign in", sign_in)
+if __name__ == "__main__":
+   run_sign_in()
