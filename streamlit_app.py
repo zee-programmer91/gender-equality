70755a0ec537b3623e3859c35f2764de53e9a530
blanga021 <blanga021@student.wethinkcode.co.za>
2022-08-06 15:28:34 +0200

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as stream
 
 stream.title("Equality Is The Goal Now")
-stream.write("Hello World!")
+stream.write(
+"""This fight is an ongoing thing that we as an organization would like you to be part of as we fight together
+to get closer to the goal that we as women have been striving for - EQUALITY!
+""")
